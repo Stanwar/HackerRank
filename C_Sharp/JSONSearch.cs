ab76313600ad9e88e90c22114fe4c35ba5dd03c5
Sharad Tanwar <sharad_tanwar@outlook.com>
2017-02-15 10:27:20 -0600

    HackerRank Practice
    Completing HackerRank using C#

---
+++ C_Sharp/JSONSearch.cs
@@ -0,0 +1,86 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JObject obj = JObject.Parse(@"{
+               'root' : { 
+                    'CPU' : 'Intel',
+                    'Drives' : {
+                        'Features' : 'Warranty',
+                        'Manufacturers'  : 'Western Digital'
+                    }, 
+                    'Screen' : 'Dell'
+                }   
+            }");
+
+            JToken jToken = obj as JToken;
+
+            bool output = BFS(jToken, "Manufacturers");
+            Console.WriteLine(output);
+            Console.Read();
+        }
+
+        public static bool BFS(JToken root, string input)
+        {
+            Queue<JToken> list = new Queue<JToken>();
+            list.Enqueue(root.First);
+
+            JToken node;
+            // Run loop till there are values in queue
+            while (list.Count > 0)
+            {
+                node = list.Dequeue();
+                JEnumerable<JToken> children = node.Children();
+
+                foreach (JToken child in children.Children<JToken>())
+                {
+                    list.Enqueue(child);
+                }
+
+                JProperty jProp = node as JProperty;
+
+                if (jProp.Name.ToString().Equals(input))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }       
+    }
+
+    public class first
+    {
+        public virtual bool doSomething()
+        {
+            return false;
+        }
+        public void doSomethingChild()
+        {
+            Console.WriteLine("Inside Parent");
+        }
+    }
+
+    class child : first
+    {
+
+        public override bool doSomething()
+        {
+            return true;
+        }
+        public void doSomethingChildValue()
+        {
+            Console.WriteLine("Inside Child");
+        }
+    }
+
+}
---
+++ C_Sharp/StringReducer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class StringReducer
+    {
+        public static void Main(String[] args)
+        {
+            string input = "aabb";
+            Console.WriteLine(Reduce(input));
+        }
+
+        public static string Reduce(string input)
+        {
+            char[] charArr = input.ToArray<char>();
+
+            if (charArr.Length == 1)
+            {
+                return charArr.ToString();
+            }else if (charArr.Length == 2 && charArr[0] == charArr[1])
+            {
+                return "Empty String";
+            }else
+            {
+                StringBuilder strBuilder = new StringBuilder(
+                    );
+                for(int i = 1; i < charArr.Length; i++)
+                {
+                    if (charArr[i-1] == charArr[i])
+                    {
+                        i = i + 1;
+                    }else
+                    {
+                        strBuilder.Append(charArr[i - 1]);
+                    }
+                }
+
+                if (strBuilder.Length>0)
+                {
+                    return strBuilder.ToString();
+                }else
+                {
+                    return "Empty String";
+                }
+            }
+            return null;
+        }
+    }
+}
