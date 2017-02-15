81a4cdf934e30540d6c89759d4db2f750a51caab
Sharad Tanwar <sharad_tanwar@outlook.com>
2017-02-15 12:20:27 -0600

    Camel Case Challenge
    Challenge was easy. Count the number of Uppercase characters.

---
+++ C_Sharp/CamelCase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class CamelCase
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            NumberOfWords(input);
+        }
+
+        public static void NumberOfWords(string input)
+        {
+            int count = 0;
+            if ((int)input.ToCharArray()[0]>=97 && (int)input.ToCharArray()[0] <= 122)
+            {
+                count = 1;
+            }
+            count = count + input.Count( x => (int)x >= 65 && (int)x <= 90);
+            Console.WriteLine(count);
+        }
+    }
+}
